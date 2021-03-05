58ff4350afcf320e0e16019038102e0d6f89ffa8
Pavel Romanenko <p.rom@me.com>
2021-03-05 02:27:28 +0100

    Update with the full Customer class and two customers

--- supermarket_visualization/tiles_skeleton.py
+++ supermarket_visualization/tiles_skeleton.py
@@ -11,38 +11,38 @@ OFS = 50
 MARKET = """
 ##################
 ##...............#
-##..DE..ES..SA..A#
-##..DE..ES..SA..A#
-##..DE..ES..SA..A#
-##..DE..ES..SA..A#
-##..DE..ES..SA..A#
+##..DR..RS..SF..F#
+##..DR..RS..SF..F#
+##..DR..RS..SF..F#
+##..DR..RS..SF..F#
+##..DR..RS..SF..F#
 ##...............#
 ##..CC..CC..CC...#
 ##...............#
 ##...............#
-##############GG##
+##WW##WW##WW##GG##
 """.strip()
 
 class Customer:
     
-    #def __init__(self, idn, state, transition_mat, terrain_map, image, x, y):
-    def __init__(self,terrain_map, image, x, y):
-        #self.id = idn
-        #self.state = state
-        #self.transition_mat = transition_mat
+    def __init__(self, idn, state, transition_mat, terrain_map, image, x, y):
+    #def __init__(self,terrain_map, image, x, y):
+        self.id = idn
+        self.state = state
+        self.transition_mat = transition_mat
 
         self.terrain_map = terrain_map
         self.image = image
         self.x = x
         self.y = y
 
-        #self.tr_array_dict = {
-        #'dairy' : self.transition_mat[0,:],
-        #'drinks' : self.transition_mat[1,:],
-        #'entry' : self.transition_mat[2,:],
-        #'fruit' : self.transition_mat[3,:],
-        #'spices' : self.transition_mat[4,:]
-        #}
+        self.tr_array_dict = {
+        'dairy' : self.transition_mat[0,:],
+        'drinks' : self.transition_mat[1,:],
+        'entry' : self.transition_mat[2,:],
+        'fruit' : self.transition_mat[3,:],
+        'spices' : self.transition_mat[4,:]
+        }
 
     def __repr__(self):
         """
@@ -53,31 +53,29 @@ class Customer:
 
     def draw(self, frame):
         xpos = OFS + self.x * TILE_SIZE
-        ypos = OFS + self.y * TILE_SIZE # Pavel: not too sure about this
-        #frame[ypos:ypos+32, xpos:xpos+32] = self.image # Pavel: absolutely not sure about this
-        frame[ypos-32:ypos, xpos-32:xpos] = self.image # Pavel: absolutely not sure about this
-        #frame[ypos:ypos, xpos:xpos] = self.image # Pavel: absolutely not sure about this
+        ypos = OFS + self.y * TILE_SIZE 
+        frame[ypos-32:ypos, xpos-32:xpos] = self.image 
         # overlay the Customer image / sprite onto the frame
 
-    #def is_active(self):
-    #    """
-    #    Returns True if the customer has not reached the checkout
-    #    for the second time yet, False otherwise.
-    #    """
-    #    if self.state != 'checkout':
-    #         return True 
-    #    if self.state == 'checkout':
-    #        return False 
+    def is_active(self):
+        """
+        Returns True if the customer has not reached the checkout
+        for the second time yet, False otherwise.
+        """
+        if self.state != 'checkout':
+             return True 
+        if self.state == 'checkout':
+            return False 
 
-    #def next_state(self):
-    #    """
-    #    Propagates the customer to the next state
-    #    using a weighted random choice from the transition probabilities
-    #    conditional on the current state.
-    #    Returns nothing.
-    #    """
-    #  
-    #    self.state = np.random.choice(['checkout', 'dairy', 'drinks', 'fruit', 'spices'], p=self.tr_array_dict[f'{self.state}'])
+    def next_state(self):
+        """
+        Propagates the customer to the next state
+        using a weighted random choice from the transition probabilities
+        conditional on the current state.
+        Returns nothing.
+        """
+      
+        self.state = np.random.choice(['checkout', 'dairy', 'drinks', 'fruit', 'spices'], p=self.tr_array_dict[f'{self.state}'])
 
 class SupermarketMap:
     """Visualizes the supermarket background"""
@@ -101,21 +99,21 @@ class SupermarketMap:
         if char == "#":
             return self.tiles[0:32, 0:32]
         elif char == "G": # gates
-            return self.tiles[7 * 32 : 8 * 32, 3 * 32 : 4 * 32] # 7th row, 3rd column
+            return self.tiles[8 * 32 : 9 * 32, 3 * 32 : 4 * 32] 
+        elif char == "W": # window
+            return self.tiles[8 * 32 : 9 * 32, 4 * 32 : 5 * 32]
         elif char == "C": # checkout
             return self.tiles[2 * 32 : 3 * 32, 8 * 32 : 9 * 32]
-        elif char == "A": # apple --> fruits
+        elif char == "F": # fruits
             return self.tiles[1 * 32 : 2 * 32, 4 * 32 : 5 * 32] 
-        elif char == "B": # banana
-            return self.tiles[0 * 32 : 1 * 32, 4 * 32 : 5 * 32] 
         elif char == "S": # spices
-            return self.tiles[1 * 32 : 2 * 32, 3 * 32 : 4 * 32] 
-        elif char == "E": # eggs --> dairy
-            return self.tiles[7 * 32 : 8 * 32, 11 * 32 : 12 * 32] 
+            return self.tiles[1 * 32 : 2 * 32, 3 * 32 : 4 * 32]       
+        elif char == "R": # dairy
+            return self.tiles[8 * 32 : 9 * 32, 7 * 32 : 8 * 32] 
         elif char == "D": # drinks
             return self.tiles[6 * 32 : 7 * 32, 13 * 32 : 14 * 32] 
-        elif char == "c": # consumers
-            return self.tiles[3 * 32 : 4 * 32, 0 * 32 : 1 * 32] 
+        elif char == "c": # customer/shopping cart
+            return self.tiles[8 * 32 : 9 * 32, 6 * 32 : 7 * 32] 
         else:
             return self.tiles[32:64, 64:96]
 
@@ -145,20 +143,22 @@ class SupermarketMap:
 if __name__ == "__main__":
 
     background = np.zeros((700, 1000, 3), np.uint8)
-    tiles = cv2.imread("tiles.png") # the cv2.imread() function reads in the image as a NumPy array, so it can be sliced within the program.
+    tiles = cv2.imread("tiles_new.png") # the cv2.imread() function reads in the image as a NumPy array, so it can be sliced within the program.
 
     market = SupermarketMap(MARKET, tiles)
     customer_image = SupermarketMap(MARKET, tiles).get_tile('c')
 
-    #customer = Customer(1, 'entry', matrix_monday, market, customer_image, 16, 2)
-    customer = Customer(market, customer_image, 4, 5)
+    customer1 = Customer(1, 'drinks', matrix_monday, market, customer_image, 4, 5)
+    customer2 = Customer(2, 'entry', matrix_monday, market, customer_image, 16, 11)
+    #customer = Customer(market, customer_image, 4, 5)
     #Customer(terrain_map, image, x, y)
 
     while True:
         frame = background.copy()
         market.draw(frame)
         
-        customer.draw(frame)
+        customer1.draw(frame)
+        customer2.draw(frame)
 
         cv2.imshow("frame", frame) # the cv2.imshow() method is what’s actually displaying each frame on the screen
 
